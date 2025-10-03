20981d4109b7c38a357fc5cf6690a6141ab90958
MartinGF28 <mgf2899@gmail.com>
2025-10-02 18:43:27 -0600

    initial

---
+++ assets/js/main.js
@@ -0,0 +1,134 @@
+const peliculas = {
+        "Amores perros": {
+          titulo: "Amores perros",
+          img: "assets/picture/amores-perros.jpg",
+          descripcion:
+            "En la Ciudad de México, un accidente de tráfico conecta tres historias: Octavio, un adolescente, decide fugarse con Susana, su cuñada. Su perro Cofi se convierte en instrumento para conseguir el dinero y poder escapar. Al mismo tiempo, Daniel abandona a su familia para irse a vivir con Valeria, una modelo. El día que ambos festejan su nueva vida, el destino hace que Valeria sufra un accidente. Al lugar llega El Chivo, un exguerrillero que, tras varios años de cárcel, ahora es asesino a sueldo.",
+          año: "2000",
+          duracion: "150 min.",
+          pais: "México",
+          direccion: "Alejandro González Iñárritu",
+          guion:"Guillermo Arriaga",
+          clip: "https://www.youtube.com/embed/lmhzALdF0mU?si=dCqfNwmjFHWllsEq",
+        },
+        "El ángel exterminador": {
+          titulo: "El ángel exterminador",
+          img: "assets/picture/angel-exterminador.jpg",
+          descripcion: "Después de una cena en la mansión de los Nóbile, los invitados descubren que, por razones inexplicables, no pueden salir del lugar. Al prolongarse la situación durante varios días, la cortesía en el trato deja paso al más primitivo y brutal instinto de supervivencia. Una parábola sobre la descomposición de una clase social encerrada en sí misma. ",
+          año: "1962",
+          duracion: "90 min.",
+          pais: "México",
+          direccion: "Luis Buñuel",
+          guion:"Luis Buñuel, Luis Alcoriza",
+          clip: "https://www.youtube.com/embed/ysYG7qaz29c?si=D3Pru_qTc8TXWLSl",
+        },
+        Godzilla: {
+          titulo: "Godzilla",
+          img: "assets/picture/godzilla.jpg",
+          descripcion: "Las pruebas nucleares realizadas por los franceses en el Pacífico provocan la aparición en el océano de un gigantesco reptil mutante, que ha sido localizado tras atacar un barco de pesca japonés. Pero lo peor es que el monstruo es anfibio y saldrá del océano para dirigirse a Nueva York, en cuyas calles causará el pánico.",
+          año: "1998",
+          duracion: "130 min.",
+          pais: "Estados Unidos",
+          direccion: "Roland Emmerich",
+          guion:"Dean Devlin, Roland Emmerich. Historia: Ted Elliott, Terry Rossio, Dean Devlin, Roland Emmerich",
+          clip: "https://www.youtube.com/embed/TAfd8SaG4T8?si=i_oxdEeBFiN_jfq7",
+        },
+        "Licencia para matar": {
+          titulo: "Licencia para matar",
+          img: "assets/picture/licencia-matar.jpg",
+          descripcion: "El agente 007 a la caza y captura de un peligroso narcotraficante latinoamericano, con implicaciones personales de por medio: el capo de la droga mató a un amigo suyo, y Bond desea vengarse.",
+          año: "1989",
+          duracion: "135 min.",
+          pais: "Reino Unido",
+          direccion: "John Glen",
+          guion:"Michael Wilson, Richard Maibaum. Personaje: Ian Fleming",
+          clip: "https://www.youtube.com/embed/quHQAuO0lkg?si=ffpDJOeanLTvIjDE",
+        },
+        Roma: {
+          titulo: "Roma",
+          img: "assets/picture/roma.jpg",
+          descripcion:"Cleo (Yalitza Aparicio) es la joven sirvienta de una familia que vive en la Colonia Roma, barrio de clase media-alta de Ciudad de México. En esta carta de amor a las mujeres que lo criaron, Cuarón se inspira en su propia infancia para pintar un retrato realista y emotivo de los conflictos domésticos y las jerarquías sociales durante la agitación política de la década de los 70.",
+          año: "2018",
+          duracion: "135 min.",
+          pais: "México",
+          direccion: "Alfonso Cuarón",
+          guion:"Alfonso Cuarón",
+          clip: "https://www.youtube.com/embed/IzqcQrFit24?si=Ur-TpDTgxWFNR1ea",
+        },
+        "Romeo + Julieta": {
+          titulo: "Romeo + Julieta",
+          img: "assets/picture/romeo-julieta.jpg",
+          descripcion: "En Verona Beach se suceden los enfrentamientos entre las dos familias más poderosas (los Montesco y los Capuleto), cuyo objetivo es conseguir el dominio de la ciudad. Fulgencio Capuleto organiza una fiesta a la que asiste, gracias a la mediación de su amigo Mercuccio, Romeo Montesco. Lo que los Capuleto pretenden es que su joven hija Julieta se comprometa con el arrogante Dave Paris, el hijo del gobernador. Pero, cuando Romeo y Julieta se miran, surgirá entre ellos desde el primer momento la chispa del amor. Adaptación moderna del clásico de Shakespeare.",
+          año: "1996",
+          duracion: "120 min.",
+          pais: "Estados Unidos",
+          direccion: "Baz Luhrmann",
+          guion:"Craig Pearce, Baz Luhrmann. Obra: William Shakespeare",
+          clip: "https://www.youtube.com/embed/ivbuYR2J-SI?si=X1vbTZfYe5UglWHg",
+        },
+        Spectre: {
+          titulo: "Spectre",
+          img: "assets/picture/spectre.jpg",
+          descripcion: "James Bond recibe un críptico mensaje del pasado que le confía una misión secreta que lo lleva a México D.F. y a Roma, donde conoce a Lucía Sciarra, la hermosa viuda de un infame criminal. Bond se infiltra en una reunión secreta y descubre la existencia de una siniestra organización conocida como SPECTRE. Mientras tanto, en Londres, el nuevo director del Centro para la Seguridad Nacional cuestiona las acciones de Bond y pone en duda la importancia del MI6, encabezado por M. De modo encubierto Bond recluta a dos colaboradores para que le ayuden a encontrar a Madeleine Swann, la hija de su gran enemigo, el Sr. White, pues quizá tenga la clave para desentrañar el misterio de SPECTRE. A medida que Bond avanza en su investigación, descubre una estremecedora conexión entre él y el enemigo que busca.",
+          año: "2015",
+          duracion: "148 min.",
+          pais: "Reino Unido",
+          direccion: "Sam Mendes",
+          guion:"John Logan, Neal Purvis, Robert Wade, Jez Butterworth. Personajes: Ian Fleming",
+          clip: "https://www.youtube.com/embed/ashLaclKCik?si=rVDWAoEswcpN-8_F",
+        },
+        "Vengador del futuro": {
+          titulo: "Vengador del futuro",
+          img: "assets/picture/licencia-matar.jpg",
+          descripcion: "La Tierra, año 2084. Doug Quaid, un hombre que lleva una vida aparentemente tranquila, vive atormentado por una pesadilla que todas las noches lo transporta a Marte. Decide entonces recurrir al laboratorio de Recall, una empresa de vacaciones virtuales que le ofrece la oportunidad de materializar su sueño gracias a un fuerte alucinógeno, pero la droga hace aflorar a su memoria una estancia verdadera en Marte cuando era el más temido agente del cruel Coohagen.",
+          año: "1990",
+          duracion: "109 min.",
+          pais: "Estados Unidos",
+          direccion: "Paul Verhoeven",
+          guion:"Dan O'Bannon, Gary Goldman, Ronald Shusett. Relato: Philip K. Dick",
+          clip: "https://www.youtube.com/embed/qJEgIxPkdB4?si=7foHUbbBMJ6n1436",
+        },
+      };
+
+      function mostrarPelicula(nombrePelicula) {
+        // Ocultar todo menos header y footer
+        document.querySelectorAll(".swiper, .mapa-section").forEach((el) => {
+          el.style.display = "none";
+        });
+
+        const data = peliculas[nombrePelicula];
+        if (data) {
+          const detalle = document.getElementById("detalle-pelicula");
+          detalle.style.display = "block";
+          detalle.querySelector(".contenido").innerHTML = `
+                <div class="poster">
+                    <h3>${data.titulo}</h3>
+                    <img src="${data.img}" alt="${data.titulo}" style="max-width:400px; display:block; margin:10px 0;">
+                </div>
+                <div class="trailer-text">
+                    <iframe width="800" height="600" src="${data.clip}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                    <p>${data.descripcion}</p>
+                    <p> <strong>Año:</strong> ${data.año}</p>
+                    <p> <strong>Duraciónn:</strong> ${data.duracion}</p>
+                    <p> <strong>País:</strong> ${data.pais}</p>
+                    <p> <strong>Dirección:</strong> ${data.direccion}</p>
+                    <p> <strong>Guión:</strong> ${data.guion}</p>
+                </div>
+            `;
+        }
+      }
+
+      document.querySelectorAll(".list-down-content a").forEach((link) => {
+        link.addEventListener("click", (e) => {
+          e.preventDefault();
+          const nombre = link.innerText.trim();
+          mostrarPelicula(nombre);
+        });
+      });
+
+      document.getElementById("home").addEventListener("click", () => {
+        document.querySelectorAll(".swiper, .mapa-section").forEach((el) => {
+          el.style.display = "";
+        });
+        document.getElementById("detalle-pelicula").style.display = "none";
+      });
